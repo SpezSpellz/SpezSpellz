78c78e4cbd9779725d8d86e32bd6047bcdda104c
Jiramate Kedmake <jiramate2548@gmail.com>
2024-09-30 20:33:55 +0700

    most basic spell model

--- spezspellz/models.py
+++ spezspellz/models.py
@@ -1,4 +1,18 @@
 """This file contains models."""
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+"""Assuming every model have an id element to it by default"""
+
+
+class Spell(models.Model):
+    userID = models.CharField(max_length=20)
+    # TODO: Will connect this to user model later
+
+    title = models.CharField(max_length=20)
+    postDescription = models.CharField(max_length=400)
+    category = models.CharField(max_length=20, null=True, blank=True)
+    tag = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        """Return the spell title"""
+        return self.title
