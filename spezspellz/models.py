621d8634a90a2e2718717284298af2d83fc2e663
Yutivich TUNUENG <yutivich2005@gmail.com>
2024-10-01 16:19:33 +0700

    Rename attributes in UserInfo and Spell models and create migration file

--- spezspellz/models.py
+++ spezspellz/models.py
@@ -8,17 +8,18 @@ class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     privacy = models.BooleanField(default=False)
     notification = models.BooleanField(default=True)
-    userDescription = models.CharField(max_length=400)
+    user_desc = models.CharField(max_length=400)
 
     def __str__(self):
         return self.user.username + " Info"
+
 
 class Spell(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     """If user is deleted his spells are also gone"""
 
     title = models.CharField(max_length=20)
-    postDescription = models.CharField(max_length=400)
+    post_desc = models.CharField(max_length=400)
     category = models.CharField(max_length=20, null=True, blank=True)
     tag = models.CharField(max_length=20, null=True, blank=True)
     image_url = models.URLField(default="https://encrypted-tbn0.gstatic.com/images?q="
