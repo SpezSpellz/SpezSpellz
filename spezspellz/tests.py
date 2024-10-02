7fc8d14154ddc30c834ccb08ef69aa5829a5ee44
Pawat Sarnchawanakit <pawat.sar@ku.th>
2024-10-02 12:10:00 +0700

    Added tests for tag search.

--- spezspellz/tests.py
+++ spezspellz/tests.py
@@ -1,5 +1,7 @@
 """This file contains tests."""
+import json
 from django.test import TestCase
+from .models import Tag
 
 
 class SimpleTest(TestCase):
@@ -9,3 +11,60 @@ class SimpleTest(TestCase):
         """Go to home page."""
         response = self.client.get("")
         self.assertEqual(response.status_code, 200)
+
+    def test_tag_search(self):
+        """Searching tag."""
+        tags = ("Fireball", "Frostbolt", "Lightning Strike", "Healing Light",
+                "Shadow Step", "Arcane Blast", "Wind Gust", "Earthquake",
+                "Divine Shield", "Summon Familiar", "Invisibility",
+                "Teleportation", "Time Warp", "Poison Cloud",
+                "Barrier of Protection")
+
+        def add_tag(name: str) -> None:
+            new_tag = Tag(name=name)
+            new_tag.save()
+
+        for tag in tags:
+            add_tag(tag)
+
+        def search_tag(query: str) -> list[str]:
+            res = self.client.post("/tags/",
+                                   json.dumps({
+                                       "method": "search",
+                                       "query": query
+                                   }),
+                                   content_type='application/json').content
+            return json.loads(res)
+
+        self.assertListEqual(sorted(search_tag("Fire")), sorted(["Fireball"]))
+        self.assertListEqual(
+            sorted(search_tag("st")),
+            sorted([
+                "Frostbolt", "Shadow Step", "Arcane Blast", "Wind Gust",
+                "Lightning Strike"
+            ]))
+        self.assertListEqual(search_tag("Unknown"), [])
+        self.assertListEqual(sorted(search_tag("")), sorted(tags))
+        res = self.client.post("/tags/",
+                               json.dumps({
+                                   "method": "search",
+                                   "query": 80085
+                               }),
+                               content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+        res = self.client.post("/tags/",
+                               json.dumps({
+                                   "method": "search",
+                               }),
+                               content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+        res = self.client.post("/tags/",
+                               json.dumps({
+                                   "method": "get",
+                               }),
+                               content_type='application/json')
+        self.assertEqual(res.status_code, 404)
+        res = self.client.post("/tags/",
+                               "\"{\"{",
+                               content_type='application/json')
+        self.assertEqual(res.status_code, 500)
--- spezspellz/views.py
+++ spezspellz/views.py
@@ -61,7 +61,7 @@ class TagsPage(View, RPCView):
             return HttpResponse("Parameter `max_len` must be an integer", status=400)
         if max_len > 100 or max_len < 1:
             return HttpResponse("Parameter `max_len` must be more than 0 but less than 100", status=400)
-        return HttpResponse(json.dumps([tag.name for tag in Tag.objects.filter(name__contains=query)[0:max_len]]), status=200)
+        return HttpResponse(json.dumps([tag.name for tag in Tag.objects.filter(name__icontains=query)[0:max_len]]), status=200)
 
 
 class ProfilePage(View):
