ae54013c181e3d63b175ee892b00227b5312bb4f
Yutivich TUNUENG <yutivich2005@gmail.com>
2024-10-26 19:21:50 +0700

    Notifications and count loaded along with page
    Bell icon appear every page when logged in
    
    Notification count disappear when click bell

--- spezspellz/views/__init__.py
+++ spezspellz/views/__init__.py
@@ -12,7 +12,7 @@ from .thumbnail_view import thumbnail_view
 from .user_settings_page import UserSettingsPage
 from .register_view import RegisterView
 from .profile_picture_view import profile_picture_view
-from .notification_view import NotificationView
+from .notification_view import NotificationView, get_notifications
 
 
 __all__ = (
@@ -32,4 +32,5 @@ __all__ = (
     "other_profile_view",
     "other_spell_view",
     "NotificationView",
+    "get_notifications",
 )
