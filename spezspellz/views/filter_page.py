591338b80e709f645d560073c7c76df2740186e0
Pawat Sarnchawanakit <pawat.sar@ku.th>
2024-10-31 20:17:36 +0700

    Make filter page not return duplicated spells Make tags in filter page AND instead of OR

--- spezspellz/views/filter_page.py
+++ spezspellz/views/filter_page.py
@@ -15,12 +15,15 @@ class FilterPage(View):
         """Handle GET requests for filter page."""
         selected_category = safe_cast(int, request.GET.get('category'), None)
         selected_tags = request.GET.getlist('tag')
-        spells = Spell.objects
+        spells = Spell.objects.all()
 
         if selected_category:
             spells = spells.filter(category__pk=selected_category)
         if selected_tags:
-            spells = spells.filter(hastag__tag__name__in=[safe_cast(str, selected_tag, '') for selected_tag in selected_tags])
+            for selected_tag in selected_tags:
+                spells = spells.filter(
+                    hastag__tag__name=safe_cast(str, selected_tag, '')
+                )
 
         query = request.GET.get("s")
         if query:
@@ -30,10 +33,11 @@ class FilterPage(View):
         if creator is not None:
             spells = spells.filter(creator__pk=creator)
 
-        spells = spells.all()
         page = safe_cast(int, request.GET.get("page"), 1)
         spell_count = spells.count()
-        max_page = spell_count//self.__class__.SPELLS_PER_PAGE + (1 if spell_count % self.__class__.SPELLS_PER_PAGE != 0 else 0)
+        max_page = spell_count // self.__class__.SPELLS_PER_PAGE + (
+            1 if spell_count % self.__class__.SPELLS_PER_PAGE != 0 else 0
+        )
 
         sort_option = request.GET.get("sort")
         if sort_option == "recent":
@@ -41,24 +45,48 @@ class FilterPage(View):
         elif sort_option == "oldest":
             spells = spells.order_by('id')
         elif sort_option == "most_commented":
-            spells = spells.annotate(comment_count=Count('spellcomment')).order_by('-comment_count')
+            spells = spells.annotate(
+                comment_count=Count('spellcomment')
+            ).order_by('-comment_count')
         elif sort_option == "least_commented":
-            spells = spells.annotate(comment_count=Count('spellcomment')).order_by('comment_count')
+            spells = spells.annotate(
+                comment_count=Count('spellcomment')
+            ).order_by('comment_count')
         elif sort_option == "most_viewed":
-            spells = spells.annotate(view_count=Count('spellhistoryentry')).order_by('-view_count')
+            spells = spells.annotate(
+                view_count=Count('spellhistoryentry')
+            ).order_by('-view_count')
         elif sort_option == "least_viewed":
-            spells = spells.annotate(view_count=Count('spellhistoryentry')).order_by('view_count')
+            spells = spells.annotate(
+                view_count=Count('spellhistoryentry')
+            ).order_by('view_count')
         elif sort_option == "most_rated":
-            spells = spells.annotate(average_rating=Avg('review__star')).order_by('-average_rating')
+            spells = spells.annotate(
+                average_rating=Avg('review__star')
+            ).order_by('-average_rating')
         elif sort_option == "least_rated":
-            spells = spells.annotate(average_rating=Avg('review__star')).order_by('average_rating')
+            spells = spells.annotate(
+                average_rating=Avg('review__star')
+            ).order_by('average_rating')
+        spells = spells.distinct()
 
-        return render(request, "filter.html", {
-            "tags": Tag.objects.all(),
-            "spell_categories": Category.objects.all(),
-            "spells": spells[self.__class__.SPELLS_PER_PAGE * (page - 1):self.__class__.SPELLS_PER_PAGE * page],
-            "selected_tags": selected_tags,
-            "cur_page": page,
-            "max_page": max_page,
-            "pages": range(max(1, page - 5), min(max_page, page + 5) + 1)
-        })
+        return render(
+            request, "filter.html", {
+                "tags":
+                Tag.objects.all(),
+                "spell_categories":
+                Category.objects.all(),
+                "spells":
+                spells[self.__class__.SPELLS_PER_PAGE *
+                       (page - 1):self.__class__.SPELLS_PER_PAGE * page],
+                "selected_tags":
+                selected_tags,
+                "cur_page":
+                page,
+                "max_page":
+                max_page,
+                "pages":
+                range(max(1, page - 5),
+                      min(max_page, page + 5) + 1)
+            }
+        )
