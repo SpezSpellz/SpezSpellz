87c8ca54f664623ad041088996de00c2a1d8d341
Jiramate Kedmake <jiramate2548@gmail.com>
2024-11-13 09:10:32 +0700

    Fix lint 2

--- spezspellz/views/login_view.py
+++ spezspellz/views/login_view.py
@@ -1,11 +1,12 @@
-"""Implement the login view"""
+"""Implement the login view."""
 from allauth.account.views import LoginView
 
 
 class CustomLoginView(LoginView):
-    """Take the user login"""
+    """Take the user login."""
+
     template_name = 'registration/login.html'
 
     def form_valid(self, form):
-        """Validate the form"""
+        """Validate the form."""
         return super().form_valid(form)
