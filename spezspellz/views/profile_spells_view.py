7c95acebc25319144d03f2c7e8b603c76e84f0bd
Pawat Sarnchawanakit <pawat.sar@ku.th>
2024-11-13 21:05:25 +0700

    Fix lints

--- spezspellz/views/profile_spells_view.py
+++ spezspellz/views/profile_spells_view.py
@@ -1,5 +1,5 @@
 """Implements the myspell page."""
-from typing import cast, Any
+from typing import cast, Any, Optional
 from django.http import HttpRequest, HttpResponseBase
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
@@ -10,7 +10,7 @@ from spezspellz.utils import get_or_none
 
 
 @login_required
-def profile_spells_view(request: HttpRequest, user_id: int = None) -> HttpResponseBase:
+def profile_spells_view(request: HttpRequest, user_id: Optional[int] = None) -> HttpResponseBase:
     """View for all created spell by current user."""
     user = request.user
     if user_id is None:
@@ -18,10 +18,11 @@ def profile_spells_view(request: HttpRequest, user_id: int = None) -> HttpRespon
     else:
         if user_id == user.pk:
             return redirect("spezspellz:profile_spell")
-        focus_user = get_or_none(User, pk=user_id)
-        if focus_user is None:
+        other_user = get_or_none(User, pk=user_id)
+        if other_user is None:
             messages.success(request, "That user does not exist.")
             return redirect('spezspell:home')
+        focus_user = other_user
     spells = cast(Any, focus_user).spell_set.all()
     return render(request, "profile_spells.html", {
         "spells": spells,
