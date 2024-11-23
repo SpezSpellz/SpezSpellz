130171c9d173db55ec5e2d6620731d3cc5d74312
Jiramate Kedmake <jiramate2548@gmail.com>
2024-11-23 19:37:50 +0700

    Make register page have the same UI as login page

--- spezspellz/views/register_view.py
+++ spezspellz/views/register_view.py
@@ -1,5 +1,7 @@
 """Implements the register page."""
 from django.urls import reverse_lazy
+from django.shortcuts import resolve_url
+from django.utils.http import urlencode
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 
@@ -8,5 +10,14 @@ class RegisterView(CreateView):
     """View for registration/signup."""
 
     form_class = UserCreationForm
-    success_url = reverse_lazy("login")
+
+    def get_success_url(self):
+        # Check if `next` is in the query parameters
+        next_url = self.request.GET.get('next')
+        if next_url:
+            # Resolve the URL to avoid invalid redirection
+            login_url = reverse_lazy("login")
+            return f"{login_url}?{urlencode({'next': next_url})}"
+        return reverse_lazy("login")  # Default fallback URL
+
     template_name = "registration/register.html"
